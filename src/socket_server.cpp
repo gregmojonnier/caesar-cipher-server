baa21b371491aad59ea07ed06afd59cbf05b4188
Greg Mojonnier <gregmojonnier@gmail.com>
2016-10-30 20:23:50 -0400

    Server parent process now just waits for client connections, forking and letting client processes handle each connection.

--- src/socket_server.cpp
+++ src/socket_server.cpp
@@ -18,45 +18,58 @@ bool SocketServer::StartServer(int port)
         return false;
     }
 
+    // parent listener loop to solely wait for client connections
+    // children processes handle the actual parsing, interpretation, and responding
+    int client_sock;
     while (true) {
         std::cout << "Waiting for a new client connection..." << std::endl;
-        int client_sock = _socket_wrapper.WaitForClientConnection(server_sock);
+        client_sock = _socket_wrapper.WaitForClientConnection(server_sock);
         if (client_sock < 0) {
             std::cout << "accepted socket was bad!" << std::endl;
-            return false;
-        }
-
-        _socket_wrapper.SetReceiveDataTimeoutInSeconds(client_sock, 1.5);
-        std::unique_ptr<IDataInterpreter> data_interpreter = std::make_unique<CaesarCipherDataInterpreter>();
-
-        while (true) {
-            char buffer[512];
-            ssize_t num_bytes = _socket_wrapper.WaitForData(client_sock, buffer, 511);
-            if (num_bytes == 0) {
-                std::cout << ((num_bytes == 0) ? "client closed connection!" : "error waiting for data!") << std::endl;
-                break;
-            } else if (num_bytes == -1) {
-                if (errno == EAGAIN || errno == EWOULDBLOCK) {
-                    std::cout << "Timed out the max number of times, closing connection!" << std::endl;
-                    _socket_wrapper.CloseConnection(client_sock);
-                    break;
-                }
-                std::cerr << "An error on reading data." << std::endl;
-                break;
+            continue;
+        } else {
+            pid_t pid = fork();
+            if (pid == -1) {
+                perror("Fork failed");
+                continue;
+            } else if (pid > 0) { // parent
+                continue;
             }
 
-            printf("message is %s\n", buffer);
-            std::vector<std::string> complete_words = data_interpreter->SplitDataIntoCompleteWords(buffer, num_bytes);
-            try {
-                std::string response = data_interpreter->InterpretWordsAndDetermineResponse(complete_words);
-                if (!response.empty()) {
-                    _socket_wrapper.SendData(client_sock, response.c_str(), response.size());
-                }
-            } catch(const InvalidShiftException& e) {
-                std::cout << e.what();
+            // let child process handle from here
+            _socket_wrapper.SetReceiveDataTimeoutInSeconds(client_sock, 1.5);
+            break;
+        }
+    }
+
+    // child data handler loop
+    std::unique_ptr<IDataInterpreter> data_interpreter = std::make_unique<CaesarCipherDataInterpreter>();
+    while (true) {
+        char buffer[512];
+        ssize_t num_bytes = _socket_wrapper.WaitForData(client_sock, buffer, 511);
+        if (num_bytes == 0) {
+            std::cout << ((num_bytes == 0) ? "client closed connection!" : "error waiting for data!") << std::endl;
+            break;
+        } else if (num_bytes == -1) {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                std::cout << "Timed out the max number of times, closing connection!" << std::endl;
                 _socket_wrapper.CloseConnection(client_sock);
                 break;
             }
+            std::cerr << "An error on reading data." << std::endl;
+            break;
+        }
+
+        std::vector<std::string> complete_words = data_interpreter->SplitDataIntoCompleteWords(buffer, num_bytes);
+        try {
+            std::string response = data_interpreter->InterpretWordsAndDetermineResponse(complete_words);
+            if (!response.empty()) {
+                _socket_wrapper.SendData(client_sock, response.c_str(), response.size());
+            }
+        } catch(const InvalidShiftException& e) {
+            std::cout << e.what();
+            _socket_wrapper.CloseConnection(client_sock);
+            break;
         }
     }
     return true;
